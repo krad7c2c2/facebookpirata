821e5a9f8eaf61e7dbd075c6474f4f04be9c69d8
krad7c2c2 <movil7@Mac-mini-de-Tarjetas.local>
2016-11-07 14:36:01 -0600

    introducción al login de Facebook y firebase

--- facebookpirata/ViewController.swift
+++ facebookpirata/ViewController.swift
@@ -10,17 +10,29 @@ import UIKit
 import Firebase
 import FBSDKLoginKit
 import FirebaseAuth
+import SwiftKeychainWrapper
 
 
 
 class ViewController: UIViewController {
 
+    
+    
+    @IBOutlet weak var txtEmail: SexiTextField!
+    @IBOutlet weak var txtContrasena: SexiTextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
    
         navigationController?.navigationBar.isHidden = true
         FIRMessaging.messaging().subscribe(toTopic: "/topics/news")
+        
+        if KeychainWrapper.standard.string(forKey: "EMAIL_UID") != nil ||  KeychainWrapper.standard.string(forKey: "FACE_UID") != nil {
+            
+        print("ya estabas logueado MMM")
+        }
         
     }
 
@@ -62,11 +74,60 @@ class ViewController: UIViewController {
                 print("No se pudo autenticar con FireVase: Error\(error.debugDescription)")
             } else {
                 print("se autentico con firebase")
+                KeychainWrapper.standard.set((user?.email)!, forKey: "FACE_UID")
+
             }
         
         })
         
     }
+    
+    
+    @IBAction func botonLogin(_ sender: UIButton) {
+        
+        if let correo = txtEmail.text, let contrasena = txtContrasena.text {
+        
+            FIRAuth.auth()?.createUser(withEmail: correo, password: contrasena, completion: {
+            user, error in
+                
+                if error != nil, let err = error as? NSError {
+                    if err.code == ERROR_PASSWORD_NOT_LONG {
+                        print("ingrear uno de mas de 6 caracteres")
+                    } else if err.code == ERROR_ACCOUNT_ALREDY_USE {
+                        print("la cuenta de correo ya esta siendo usado")
+                       
+                        FIRAuth.auth()?.signIn(withEmail: correo, password: contrasena, completion: {
+                            user, error in
+                            if error != nil, let err = error as? NSError {
+                                if err.code == ERROR_INVALID_PASSWORD {
+                                    print("El password ingresado no es valido")
+                                } else {
+                                    //ir al siguiente VC
+                                    print("login email exitoso")
+                                    KeychainWrapper.standard.set(correo, forKey: "EMAIL_UID")
+                                }
+                            } else {
+                                //ir al siguiente VC
+                                print("login email exitoso")
+                                KeychainWrapper.standard.set(correo, forKey: "EMAIL_UID")
+                            }
+                        })
+                        
+                    }
+                } else {
+                    print("usuario\(user?.displayName)")
+                    KeychainWrapper.standard.set(correo, forKey: "EMAIL_UID")
+                    //ir al siguiente VC
+                }
+            
+            })
+            
+        } else {
+            print("facor de introducir usuario o contraseña")
+        }
+        
+    }
+    
     
 }
 
